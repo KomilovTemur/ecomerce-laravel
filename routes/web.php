d267d23d6e9707cc582df4035ace050207278919
tim_coder <itteamdev74@gmail.com>
2023-03-30 21:25:16 +0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+});  
+
+Route::resource("/products", ProductController::class);
