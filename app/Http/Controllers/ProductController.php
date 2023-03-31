4c9c77c81fb63392d6dffc06c4e9430dada40559
tim_coder <itteamdev74@gmail.com>
2023-03-31 15:11:05 +0500

    Products controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,55 +1,97 @@
 <?php
 
-namespace App\Http\Controllers;
+  namespace App\Http\Controllers;
 
-use App\Models\Product;
-use Illuminate\Http\Request;
+  use App\Models\Product;
+  use Illuminate\Http\Request;
 
 
-class ProductController extends Controller
-{
-  public function index(Request $request)
+  class ProductController extends Controller
   {
-    $keyword = $request->get('search');
-    $perPage = 5;
-
-    $product = Product::orderby("created_at")->get();
-    if (!empty($keyword)) {
-      $product = Product::where('name', "LIKE", "%$keyword")
-        ->orWhere('category', "LIKE", "%$keyword")->latest()->paginate($perPage);
-    } else {
-      $product = Product::latest()->paginate($perPage);
+    public function index(Request $request)
+    {
+      $keyword = $request->get('search');
+      $perPage = 5;
+      $product = Product::orderby("created_at")->get();
+      if (!empty($keyword)) {
+        $product = Product::where("name", "LIKE", "%$keyword")
+          ->orWhere('category', "LIKE", "%$keyword")
+          ->latest()->paginate($perPage);
+      } else {
+        $product = Product::latest()->paginate($perPage);
+      }
+      return view("products.index", ["products" => $product])->with('i', (request()->input('page', 1) - 1) * 6);
     }
-    return view("products.index", ["products" => $product])->with('i', (request()->input('page', 1) - 1) * 6);
-  }
-  public function create()
-  {
-    return view("products.create");
-  }
-  public function store(Request $request)
-  {
 
-    $request->validate([
-      'name' => "required",
-      'image' => "required|image|mimes:jpg, png, jpeg, gif, svg|max:2028",
-    ]);
+    public function create()
+    {
+      return view("products.create");
+    }
 
-    $file_name = time() . "." . request()->image->getClientOriginalExtension();
-    $request->image->move(public_path('images'), $file_name);
+    public function store(Request $request)
+    {
 
-    $product = new Product;
-    $product->name = $request->name;
-    $product->description = $request->description;
-    $product->image = $file_name;
-    $product->category = $request->category;
-    $product->quantity = $request->quantity;
-    $product->price = $request->price;
+      $request->validate([
+        'name' => "required",
+        'image' => "required|image|mimes:jpg, png, jpeg, gif, svg|max:2028",
+      ]);
 
-    $product->save();
-    return redirect()->route('products.index')->with('success', 'Product Added successfuly');
+      $file_name = time() . "." . request()->image->getClientOriginalExtension();
+      $request->image->move(public_path('images'), $file_name);
+
+      $product = new Product;
+      $product->name = $request->name;
+      $product->description = $request->description;
+      $product->image = $file_name;
+      $product->category = $request->category;
+      $product->quantity = $request->quantity;
+      $product->price = $request->price;
+
+      $product->save();
+      return redirect()->route('products.index')->with('success', 'Product Added successfuly');
+    }
+
+    public function edit($id)
+    {
+      $product = Product::findOrFail($id);
+      return view("products.edit", ['product' => $product]);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+      $request->validate([
+        'name' => "required",
+      ]);
+      $file_name = $request->hidden_product_image;
+
+      if ($request->image != "") {
+        $file_name = time() . "." . request()->image->getClientOriginalExtension();
+        $request->image->move(public_path('images'), $file_name);
+      }
+
+      $product = Product::find($request->hidden_id);
+
+      $product->name = $request->name;
+      $product->description = $request->description;
+      $product->image = $file_name;
+      $product->category = $request->category;
+      $product->quantity = $request->quantity;
+      $product->price = $request->price;
+
+      $product->save();
+
+      return redirect()->route('products.index')->with('success', "Product has been updated successfully");
+    }
+
+    public function destroy($id)
+    {
+      $product = Product::findOrFail($id);
+      $image_path = public_path() . "/images/";
+      $image = $image_path . $product->image;
+      if (file_exists($image)) {
+        @unlink($image);
+      }
+      $product->delete();
+      return redirect('products')->with("success", "Product deleted!");
+    }
   }
-  public function edit($id) {
-    $product = Product::findOrFail($id);
-    return view("products.edit", ['product' => $product]);
-  }
-}
